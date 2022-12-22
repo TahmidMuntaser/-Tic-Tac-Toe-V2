8007f2885977d4886158ddd5d3360b2e84385d1e
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2022-12-22 18:09:20 +0600

    Interface Update

--- main.cpp
+++ main.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+
+    cout<<"             Tic-Tac-Toe"<<endl;
+    cout<<"*********************************************"<<endl;
+    cout<<"\tPlayer1[X]";
+    cout<<"\tPlayer2[O]"<<endl<<endl<<endl;
+
+    //Interface..
+
+    cout<<"\t          |     |     "<<endl;
+    cout<<"\t       1  |  2  |  3  "<<endl;
+    cout<<"\t     _____|_____|_____"<<endl;
+    cout<<"\t          |     |     "<<endl;
+    cout<<"\t       4  |  5  |  6  "<<endl;
+    cout<<"\t     _____|_____|_____"<<endl;
+    cout<<"\t          |     |     "<<endl;
+    cout<<"\t       7  |  8  |  9  "<<endl;
+    cout<<"\t          |     |     "<<endl;
+    cout<<"\n\n\n";
+
+    cout<<"\tPlayer1[X] turn: ";
+  
     
     return 0;
 }
